7cf4a2d1abd6a92d545f3d305b08c3311d9d8b7f
Rostislav <37041700+itradio@users.noreply.github.com>
2019-02-12 20:02:19 +0300

    MAIN CODE FOR TEST SERIAL
    BETWEEN ARDUINO MEGA AND UNO

---
+++ RobotRescue/Test Robot/ArduinoCode/MainCodeArduino.ino
@@ -0,0 +1,19 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial firstSerial(18, 19);
+
+void setup(){
+  Serial.begin(9600);
+  firstSerial.begin(9600);
+}
+
+void loop(){
+  if (Serial.available() && firstSerial.available()){
+    while (Serial.available() && firstSerial.available()){
+      for(int i = 0; i < 4; i++){
+        firstSerial.write(i);
+        Serial.write(firstSerial.read());
+      }
+    }
+  }
+}
