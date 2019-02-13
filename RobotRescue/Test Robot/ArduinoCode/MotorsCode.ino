5f5f64e0cd09ab05c0798e8a51fceed0e97b75b4
Rostislav <37041700+itradio@users.noreply.github.com>
2019-02-13 16:41:32 +0300

    add ultrasonic reader function
    function for reading from sensor ultrasonic\

--- RobotRescue/Test Robot/ArduinoCode/MotorsCode.ino
+++ RobotRescue/Test Robot/ArduinoCode/MotorsCode.ino
@@ -11,12 +11,16 @@ int speedMotors = 255;
 Encoder myEncoder(2,3);
 long oldPosition = -999;
 
+int ultraSonicPins[][] = {{11, 12}}; // четный номер - trig, нечетный - echo
+
 SoftwareSerial firstSerial(0, 1);
 
-void stopGoing(int firstSpeedMotor, int secondSpeedMotor){
+void stopGoing(int firstSpeedMotor, int secondSpeedMotor, int dir_pin_first, int dir_pin_second){
   analogWrite(firstSpeedMotor, 0);
   analogWrite(secondSpeedMotor, 0);
-}
+  digitalWrite(dir_pin_first, LOW);
+  digitalWrite(dir_pin_second, LOW);
+} 
 
 void back(int firstSpeedMotor, int secondSpeedMotor, int dirFirstMotor, int dirSecondMotor){
   digitalWrite(dirFirstMotor, LOW);
@@ -46,6 +50,8 @@ void turnRight(int firstSpeedMotor, int secondSpeedMotor, int dirFirstMotor, int
   digitalWrite(dirSecondMotor, HIGH);
 }
 
+
+
 void setup(){
   Serial.begin(9600);
   firstSerial.begin(9600);
@@ -55,6 +61,38 @@ void setup(){
   }
 
   forward(SPEED_FIRST_MOTOR, SPEED_SECOND_MOTOR, DIR_FIRST_MOTOR, DIR_SECOND_MOTOR);
+
+
+  // инициалиация пинов дальномеров
+  for (int i = 0; i < ultraSonicPins.length; i++){
+    for (int j = 0; j < ultraSonicPin[i].length; j++){
+      if (j == 0 || j % 2 != 0)
+        pinMode(ultraSonicPin[i][j], INPUT);
+      else pinMode(ultraSonicPin[i][j], OUTPUT);
+    }
+  }
+}
+
+int getLengthUltra(int pinEcho, int pinTrig){
+  unsigned int distationInfo = 0;
+  unsigned int signalInfo = 0;
+
+  digitalWrite(pinTrig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pinTrig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pinTrig, LOW);
+
+  signalInfo = pulseIn(pinEcho, HIGH);
+  distation = signalInfo / 58;
+
+  delay(100);
+
+  return distation;
+} 
+
+int readerFromUltrasonics(){
+  
 }
 
 void loop(){
@@ -79,6 +117,7 @@ void loop(){
         break;
       }
     }
+    
     delay(1000);
   }
 }
